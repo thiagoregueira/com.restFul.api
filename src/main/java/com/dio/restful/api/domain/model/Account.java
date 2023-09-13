a21b8de20c227dc11f26fde3dae9ecd1fc39c0cc
Thiago Regueira <thiago.regueira@yahoo.com.br>
2023-09-13 15:58:32 -0300

    correcao do precision e scale do additional limit que estava ao inverso.

--- src/main/java/com/dio/restful/api/domain/model/Account.java
+++ src/main/java/com/dio/restful/api/domain/model/Account.java
@@ -23,7 +23,7 @@ public class Account {
     @Column(precision = 13, scale = 2)
     private BigDecimal balance;
 
-    @Column(name = "additional_limit", precision = 2, scale = 13)
+    @Column(name = "additional_limit", precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
