26bbb1dc653eaf63d39ce37d92f0266ca5d12bcb
Thiago Regueira <thiago.regueira@yahoo.com.br>
2023-09-13 15:43:50 -0300

    Criação do controller e adição da dapendencia da openApi (swagger).

---
+++ src/main/java/com/dio/restful/api/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.dio.restful.api.controller;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.dio.restful.api.domain.model.User;
+import com.dio.restful.api.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        var userCreated = userService.create(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(userCreated);
+    }
+}
